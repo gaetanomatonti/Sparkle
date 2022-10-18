2840265a1885df57d2bac15d10e14e11ac1f8d00
gaetanomatonti <gaetanomatonti@icloud.com>
2022-10-18 08:21:31 +0200

    Fix RawText indentation

--- Sources/SparkleHTML/HTMLRenderer.swift
+++ Sources/SparkleHTML/HTMLRenderer.swift
@@ -16,6 +16,9 @@ public struct HTMLRenderer {
 
   // MARK: - Functions
 
+  /// Renders the specified `Component` into an HTML `String`.
+  /// - Parameter component: The component to render.
+  /// - Returns: A `String` of the component's HTML representation.
   public func render(_ component: Component) -> String {
     if let component = component as? RawText {
       return render(component, indentation: indentation)
@@ -40,6 +43,11 @@ public struct HTMLRenderer {
     return render(component.body)
   }
 
+  /// Renders the specified `Page` into an HTML `String`.
+  /// - Parameters:
+  ///   - page: The page to render.
+  ///   - indentation: The indentation to apply to the HTML.
+  /// - Returns: A `String` of the page's HTML representation.
   @StringBuilder
   private func render(_ page: Page, indentation: Indentation) -> String {
     "<!DOCTYPE html>"
@@ -58,12 +66,22 @@ public struct HTMLRenderer {
     )
   }
 
+  /// Renders the specified `Group` into an HTML `String`.
+  /// - Parameters:
+  ///   - group: The group to render.
+  ///   - indentation: The indentation to apply to the HTML.
+  /// - Returns: A `String` of the group's HTML representation.
   private func render(_ group: Group, indentation: Indentation) -> String {
     group.components.reduce("") { partialResult, component in
       partialResult + HTMLRenderer(indentation: indentation).render(component)
     }
   }
 
+  /// Renders the specified `Element` into an HTML `String`.
+  /// - Parameters:
+  ///   - element: The element to render.
+  ///   - indentation: The indentation to apply to the HTML.
+  /// - Returns: A `String` of the element's HTML representation.
   @StringBuilder
   private func render(_ element: Element, indentation: Indentation) -> String {
     if indentation.shouldInsertNewline {
@@ -87,22 +105,35 @@ public struct HTMLRenderer {
     }
   }
 
+  /// Renders the specified `Text` into an HTML `String`.
+  /// - Parameters:
+  ///   - text: The text to render.
+  ///   - indentation: The indentation to apply to the HTML.
+  /// - Returns: A `String` of the text's HTML representation.
   @StringBuilder
   private func render(_ text: Text, indentation: Indentation) -> String {
+    indentation.description
+
+    HTMLRenderer(indentation: indentation).render(text.body)
+  }
+
+  /// Renders the specified `RawText` into an HTML `String`.
+  /// - Parameters:
+  ///   - rawText: The raw text to render.
+  ///   - indentation: The indentation to apply to the HTML.
+  /// - Returns: A `String` of the raw text's HTML representation.
+  @StringBuilder
+  private func render(_ rawText: RawText, indentation: Indentation) -> String {
     if indentation.shouldInsertNewline {
       "\n"
     }
 
     indentation.description
 
-    HTMLRenderer(indentation: indentation).render(text.body)
+    rawText.content
 
     if indentation.shouldInsertNewline {
       "\n"
     }
-  }
-
-  private func render(_ rawText: RawText, indentation: Indentation) -> String {
-    rawText.content
   }
 }
